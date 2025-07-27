4fb152d4376b6a17194697605fceff89c21c302c
Jan-Eric Enlund <jan-eric.enlund@multi.fi>
2025-07-27 19:56:48 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<!-- /src/App.vue -->
+<script setup>
+// Denna fil är rotkomponenten för hela Vue-applikationen.
+// För tillfället importerar och renderar den endast vår HomePage
+// för att verifiera att hela byggkedjan fungerar.
+// Senare kommer denna fil att innehålla <router-view> för att hantera sidnavigation.
+import HomePage from './pages/home/HomePage.vue'
+</script>
+
+<template>
+  <HomePage />
+</template>
+
+<style scoped>
+/* Här kan globala, men scopade, stilar för app-ramverket läggas till senare. */
+</style>
+<!-- /src/App.vue -->
