b01b98b50943bbe3dcb7a282cb75d4373c562b2c
Jan-Eric Enlund <jan-eric.enlund@multi.fi>
2025-07-27 19:55:38 +0300

    Create main.js

---
+++ src/app/main.js
@@ -0,0 +1,11 @@
+// /src/app/main.js
+// Detta är applikationens huvudsakliga startpunkt.
+// Koden här importerar 'createApp' från Vue och vår rotkomponent 'App.vue'.
+// Därefter skapar den en ny Vue-applikation med App.vue som grund
+// och monterar den på DOM-elementet med id="app" i vår index.html.
+
+import { createApp } from 'vue'
+import App from '../../App.vue'
+
+createApp(App).mount('#app')
+// /src/app/main.js
